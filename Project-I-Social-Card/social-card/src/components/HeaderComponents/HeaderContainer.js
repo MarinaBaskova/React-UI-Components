d44501f639d3d5964b373a40e32e48ef655c2594
Marina Baskova <marinabaskova24@gmail.com>
2019-03-18 14:03:04 -0700

    added header container

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,20 @@
 import React from 'react';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 import './Header.css';
+
+const HeaderContainer = () => {
+    return (
+        <div className="headerCard">
+            <div className="topHeaderText">
+                <ImageThumbnail />
+                <HeaderTitle />
+            </div>
+            <HeaderContent />
+        </div>
+    )
+};
+
+export default HeaderContainer;
+
