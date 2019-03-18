7e6bfb4afcf5c741977bd80f61252fb1aec7bf7c
Marina Baskova <marinabaskova24@gmail.com>
2019-03-18 15:53:48 -0700

    added footer component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Header.css';
+import logo from './logo.png'
 
 
 const ImageThumbnail = () => {
-        return (<div className="logo"><img className="logoImage" alt="lambda logo" src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"></img></div>);
+        return (<div className="logo"><img className="logoImage" alt="lambda logo" src={logo}></img></div>);
 }
 
 export default ImageThumbnail;
